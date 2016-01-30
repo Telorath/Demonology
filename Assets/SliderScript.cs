5c79f210ec9c26328422db1e149398f3dd3cd00b
Kyle Bennett <TelorathSennith@gmail.com>
2016-01-30 12:49:20 -0500

    Begin Modify Health Bars

---
+++ Assets/SliderScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SliderScript : MonoBehaviour {
+
+    float value
+    {
+     set
+        {
+            GameObject temp = GameObject.Find("Slider (Script)");
+           // temp.
+        }   
+    }
+
+}
