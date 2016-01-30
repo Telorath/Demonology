28d83aeaaa70f44b9fe0d4ec5d88762d729226f4
Kyle Bennett <TelorathSennith@gmail.com>
2016-01-30 00:44:00 -0500

    Add Basic Drag Script

--- Assets/HealthScript.cs
+++ Assets/HealthScript.cs
@@ -5,6 +5,7 @@ public class HealthScript : MonoBehaviour {
 
     public int health;
     public int maxhealth;
+    bool alive = true;
     public int Health
     {
         get
@@ -18,6 +19,21 @@ public class HealthScript : MonoBehaviour {
             {
                 health = maxhealth;
             }
+            if (health <= 0)
+            {
+                alive = false;
+            }
+        }
+    }
+    public int MaxHealth
+    {
+        get
+        {
+            return maxhealth;
+        }
+        set
+        {
+            maxhealth = value;
         }
     }
 }
