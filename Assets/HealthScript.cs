8512ba22e4afc6bbd9d7e327a331a3c871f694c9
Kyle Bennett <TelorathSennith@gmail.com>
2016-01-29 23:59:51 -0500

    Add Player Object RIGHT

---
+++ Assets/HealthScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthScript : MonoBehaviour {
+
+    public int health;
+    public int maxhealth;
+    public int Health
+    {
+        get
+        {
+            return health;
+        }
+        set
+        {
+            health = value;
+            if (health > maxhealth)
+            {
+                health = maxhealth;
+            }
+        }
+    }
+}
