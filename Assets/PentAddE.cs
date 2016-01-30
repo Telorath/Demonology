a9cf36c355673ee4f381a1ed7a3ee9fce3a341b0
Chris Kennedy <chriskiller93.3@gamail.com>
2016-01-30 15:37:08 -0800

    UpDate on PentAddE

--- Assets/PentAddE.cs
+++ Assets/PentAddE.cs
@@ -12,6 +12,7 @@ public class PentAddE : MonoBehaviour
 	void ResetCatalysts()
 	{
 		Catalysts.Remove(0);		//Loss what is already in
+									//Just as we wanted it
 	}
 
 	// Use this for initialization
@@ -35,50 +36,62 @@ public class PentAddE : MonoBehaviour
 	void AddAir()
 	{
 		if (Catalysts.Length < MaxStored)
-			Catalysts += 'A';			//Just add the 1st Letter of E
+			Catalysts += 'A';			//Just add the 1st Letter of A
 	}
 	void AddFire()
 	{
 		if (Catalysts.Length < MaxStored)
-			Catalysts += 'F';			//Just add the 1st Letter of E
+			Catalysts += 'F';			//Just add the 1st Letter of F
 	}
 	void AddWater()
 	{
 		if (Catalysts.Length < MaxStored)
-			Catalysts += 'W';			//Just add the 1st Letter of E
+			Catalysts += 'W';			//Just add the 1st Letter of W
 	}
 	void AddShadow()
 	{
 		if (Catalysts.Length < MaxStored)
-			Catalysts += 'S';			//Just add the 1st Letter of E
+			Catalysts += 'S';			//Just add the 1st Letter of S
 	}
 
-	void Cast()
+	string Cast()
 	{
 		// Icicle
 		if(Catalysts == "AW" || Catalysts == "WA")
 		{
-
+			string Icicle = "Icicle";
+			ResetCatalysts();
+			return Icicle;
 		}
 		// Lightning Strike
 		if (Catalysts == "EA" || Catalysts == "AE")
 		{
-
+			string Lightning = "Lightning";
+			ResetCatalysts();
+			return Lightning;
 		}
 		// Ground spike
 		if (Catalysts == "ES" || Catalysts == "SE")
 		{
-
+			string Spike = "Spike";
+			ResetCatalysts();
+			return Spike;
 		}
 		// Fireball
 		if (Catalysts == "FS" || Catalysts == "SF")
 		{
-
+			string Fireball = "Fireball";
+			ResetCatalysts();
+			return Fireball;
 		}
 		// Magic Missile
-		if (Catalysts == "SFA" )
+		if (Catalysts.Contains("SFA"))
 		{
-
+			string MagicMissile = "MagicMissile";
+			ResetCatalysts();
+			return MagicMissile;
 		}
+		else
+			return "Melee";
 	}
 }
