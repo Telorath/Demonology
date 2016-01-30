158c3375c1f0461349ab30ac6dfdba735f5fe985
Kyle Bennett <TelorathSennith@gmail.com>
2016-01-29 21:00:14 -0500

    Add Timer Bar

---
+++ Assets/BattleScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Timers;
+
+public class BattleScript : MonoBehaviour {
+    public GameObject PlayerHealth;
+    public GameObject EnemyHealth;
+    public float xshift;
+    float pRed = 1;
+    float pBlue = 1;
+    float pGreen = 1;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    public void Turn()
+    {
+        pBlue -= 0.03f;
+        pGreen -= 0.03f;
+        PlayerHealth.transform.localScale -= new Vector3(1, 0, 0);
+        PlayerHealth.transform.localPosition -= new Vector3(xshift,0,0);
+        PlayerHealth.GetComponent<SpriteRenderer>().color = new Color(pRed,pGreen,pBlue);
+    }
+}
---
+++ Assets/Timerscript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Timerscript : MonoBehaviour {
+    public GameObject BattleObj;
+    public int TimerDuration;
+    public float baseyshift;
+    public float incyshift;
+    int ticks;
+    Vector3 basescale;
+    Vector3 basepos;
+	// Use this for initialization
+	void Start () {
+        basescale = transform.localScale;
+        basepos = transform.localPosition;
+        transform.localScale = new Vector3(basescale.x,0,basescale.z);
+        transform.localPosition = new Vector3(basepos.x, basepos.y - baseyshift, basepos.z);
+	}
+	void FixedUpdate()
+    {
+        if (TimerDuration <= 0)
+        {
+            return;
+        }
+        ticks++;
+        transform.localScale += new Vector3(0, (basescale.y / TimerDuration), 0);
+        transform.localPosition += new Vector3(0, incyshift, 0);
+        if (ticks > TimerDuration)
+        {
+            transform.localScale = new Vector3(basescale.x, 0, basescale.z);
+            transform.localPosition = new Vector3(basepos.x, basepos.y - baseyshift, basepos.z);
+            BattleObj.GetComponent<BattleScript>().Turn();
+            ticks = 0;
+        }
+    }
+	// Update is called once per frame
+	void Update () {
+
+    }
+}
