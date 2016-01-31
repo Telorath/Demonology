bcb21c241a5bdc9861219c86d65ad47bae800447
Kyle Bennett <TelorathSennith@gmail.com>
2016-01-30 20:10:00 -0500

    Add Scene Transition

---
+++ Assets/StartButtonActivate.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class StartButtonActivate : MonoBehaviour {
+
+
+    public void Activate()
+    {
+        SceneManager.LoadScene("BattleScene");
+    }
+
+}
