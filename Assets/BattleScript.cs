c4c952399b19ac6c4d6f730d4132338415203da5
Kyle Bennett <TelorathSennith@gmail.com>
2016-01-30 21:41:23 -0500

    Update Battle Script

--- Assets/BattleScript.cs
+++ Assets/BattleScript.cs
@@ -2,32 +2,40 @@
 using System.Collections;
 using System.Timers;
 
-public class BattleScript : MonoBehaviour {
+public class BattleScript : MonoBehaviour
+{
     public GameObject Player;
     public GameObject Enemy;
     public GameObject Pentagram;
     public GameObject Timer;
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
     public void Turn()
     {
-        Player.GetComponent<HealthScript>().Health -= 10;
         PlayerAttack();
+        if (Enemy.GetComponent<HealthScript>().Alive)
+            EnemyAttack();
         Timer.GetComponent<Timerscript>().ResetTimer();
     }
     public void PlayerAttack()
     {
         string s = Pentagram.GetComponent<PentAddE>().Cast();
-        if (string.Compare(s,"Melee",true) == 0)
+        if (string.Compare(s, "Melee", true) == 0)
         {
             Enemy.GetComponent<HealthScript>().Health -= 10;
         }
+    }
+    public void EnemyAttack()
+    {
+        Player.GetComponent<HealthScript>().Health -= Enemy.GetComponent<DamageScript>().damage;
     }
 }
---
+++ Assets/DamageScript.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DamageScript : MonoBehaviour {
+
+    public int damage = 20;
+
+}
